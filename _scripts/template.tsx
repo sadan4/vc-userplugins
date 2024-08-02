b46817f996be98afe3877b28ddf766dba05a9162
sadan <117494111+sadan4@users.noreply.github.com>
2024-08-01 22:25:11 -0400

    create scripts

---
+++ _scripts/template.tsx
@@ -0,0 +1,12 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "PLUGIN_NAME",
+    description: "",
+    authors: [
+        {
+            name: "sadan",
+            id: 521819891141967883n
+        }
+    ]
+})
