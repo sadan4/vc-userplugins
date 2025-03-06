c6994f1bce1fbc6f3644b4a4a9e547f3c5202bff
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-06 18:44:37 -0500

    update template

--- _scripts/template.tsx
+++ _scripts/template.tsx
@@ -1,12 +1,14 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
     name: "PLUGIN_NAME",
     description: "",
-    authors: [
-        {
-            name: "sadan",
-            id: 521819891141967883n
-        }
-    ]
-})
+    authors: [Devs.sadan]
+});
